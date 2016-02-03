0d353663e1ea35159390738fdcb201942f29c930
PABLO A. LOPEZ DOMOWICZ <palopezd@us.ibm.com>
2016-02-02 23:32:17 -0500

    added new url  to front end config

--- src/config.js
+++ src/config.js
@@ -4,10 +4,11 @@ const config = {
 		clientId: '98707c5e5bcd0b28fec8',
 		apiUrl: 'http://localhost:5000'
  	},
-	'labelr.surge.sh':{
-		authUrl: 'xx',
-		clientId: 'xx'
-	}
+ 	"grouplayv1.herokuapp.com":{
+ 		authUrl: 'https://tester-localhost.herokuapp.com/authenticate/',
+		clientId: '98707c5e5bcd0b28fec8',
+		apiUrl: 'https://grouplayv1.herokuapp.com'	
+ 	}
 }[window.location.hostname]
 
 export default config
