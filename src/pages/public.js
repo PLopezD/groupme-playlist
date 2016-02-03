7440eef69e1c554f9d95543663fbb14c2b7bd564
PABLO A. LOPEZ DOMOWICZ <palopezd@us.ibm.com>
2016-02-03 07:22:05 -0500

    styled boxes with futurastic border

--- src/pages/public.js
+++ src/pages/public.js
@@ -11,13 +11,12 @@ export default React.createClass({
 		const {posts} = this.props;
 		if (posts){
 			postHtml = posts.map((post)=>{
-				return(<div>
-					<div className="link" key={post.id}> 	
-						<a target="_blank" href={post.text}>{post.description || post.text}
-						</a>
-					</div>
-					<img src={post.avatar_url}></img>		
-					</div>
+				return(<div className='linkContainer'>
+							<div className="link" key={post.id}> 	
+								<a target="_blank" href={post.text}>{post.description || post.text}</a>
+							</div>
+								<img src={post.avatar_url}></img>		
+						</div>
 					)
 			}
 			)
@@ -27,15 +26,14 @@ export default React.createClass({
 		}
 		return (
 			<ClickHandler className='container'>
-			<header role='banner'>
-			<h1>GMe Playlist 1.0</h1>
-			</header>
-			<div>
-			<h2>Beats</h2>
-			<ul>	
-			{postHtml}
-			</ul>
-			</div>
+				<header role='banner' className='title'>
+					<h1>GMe Playlist 1.0</h1>
+				</header>
+				<div>
+					<ul>	
+					{postHtml}
+					</ul>
+				</div>
 			</ClickHandler>
 			)	
 	}
